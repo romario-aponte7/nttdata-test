c78b3e0f6c95c716af3b5ba538b2b3be96fa03f0
romario <romario151996@gmail.com>
2023-07-30 16:09:25 -0500

    first commit

---
+++ src/main/java/com/nttdatates/NttdataTesApplication.java
@@ -0,0 +1,13 @@
+package com.nttdatates;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NttdataTesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NttdataTesApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nttdatates/NttdataTesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nttdatates;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NttdataTesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
